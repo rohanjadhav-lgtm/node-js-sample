defaba4dd04f47a1b6ed8a2f72dc7fa2c61e1e76
rohanjadhav-lgtm <rohan.jadhav@ashnik.com>
2026-02-11 16:41:06 +0530

    Update index.js
    Signed-off-by: rohanjadhav-lgtm <rohan.jadhav@ashnik.com>

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ require('elastic-apm-node').start({
   captureBody: 'all',
   logLevel: 'info'
 });
-var express = require('express')
-var app = express()
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+const express = require('express');
+const app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+const PORT = process.env.PORT || 8080;
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Node app is running on port ${PORT}`);
+});
